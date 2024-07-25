ab42a140c23280f80585e6a4821646c4b4a02184
HFBEAST <yyp82067406@gmail.com>
2024-07-25 17:36:32 +0900

    20240722 resizable

--- src/components/AnswerView.vue
+++ src/components/AnswerView.vue
@@ -23,6 +23,7 @@
     </el-row>
     <div class="answers-container">
       <el-table class="header-table"
+                border resizable
           :data="filteredAnswers"
                 style="width: 100%; max-height: 600px; height: 800px; overflow-y: auto; overflow-x: hidden;"
                 :header-cell-style="{color: '#515151', fontSize: '12px', textAlign: 'center'}"
